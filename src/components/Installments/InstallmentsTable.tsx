7e17248eae39323612b8f5b49518630823e19e55
malidaddydev <abdulahad010274@gmail.com>
2026-09-07 22:41:59 +0500

    fd

--- src/components/Installments/InstallmentsTable.tsx
+++ src/components/Installments/InstallmentsTable.tsx
@@ -607,7 +607,7 @@ export default function InstallmentsTable({ data, onPay, selectedIds = [], onSel
                                                                                  <div>
                                                                                      <span className="text-sm font-black text-red-500">{pkr(remAmt + (inst.arrears || 0))}</span>
                                                                                      {hasArrears ? (
-                                                                                         <p className="text-[9px] text-gray-400">({pkr(remAmt)} + {pkr(inst.arrears)} arr.)</p>
+                                                                                         <p className="text-[9px] text-gray-400">({pkr(remAmt)} + {pkr(inst.arrears || 0)} arr.)</p>
                                                                                      ) : null}
                                                                                  </div>
                                                                              ) : isPaid ? (
